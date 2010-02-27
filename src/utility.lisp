40b7f79f849e8c75d5c15d70a121badc62346cf5
Moskvitin Andrey <archimag@gmail.com>
2010-02-28 01:13:34 +0300

    removed obsolete code

--- src/utility.lisp
+++ src/utility.lisp
@@ -9,102 +9,6 @@
 
 (defparameter *basepath* (asdf:component-pathname (asdf:find-system :rulisp)))
 
-;;; misc
-
-(defmacro with-rulisp-db (&body body)
-  `(postmodern:with-connection *rulisp-db*
-     ,@body))
-
-(defparameter *re-email-check* 
-  "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$")
-
-
-;;; mail
-
-(defvar *sendmail*
-  (find-if #'fad:file-exists-p
-           (list "/usr/bin/sendmail"
-                 "/usr/sbin/sendmail")))
-
-(defun send-mail (to head content)
-  #+sbcl(let* ((sendmail-process (sb-ext:run-program *sendmail*
-                                                     to
-                                                     :input :stream
-                                                     :output nil
-                                                     :error nil
-                                                     :wait nil))
-               (sendmail (sb-ext:process-input sendmail-process)))
-          (unwind-protect
-               (progn
-                 (iter (for head-line in (acons "To" (format nil "~{~A ~}" to)  head))
-                       (format sendmail
-                               "~A: ~A~%"
-                               (car head-line)
-                               (cdr head-line)))
-                 (format sendmail "Content-Type: text/html; charset=\"utf-8\"~%~%")
-                 (typecase content
-                   (xtree::libxml2-cffi-object-wrapper (xtree:serialize content sendmail))
-                   (string (write-string content sendmail))
-                   (pathname (write-string (alexandria:read-file-into-string content) sendmail)))
-                 t)
-            (close sendmail)
-            (sb-ext:process-wait sendmail-process)
-            (sb-ext:process-close sendmail-process))))
-
-(defun prepare-subject (subject &optional (external-format :utf-8))
-  (format nil
-          "=?~A?B?~A?="
-          external-format
-          (base64:string-to-base64-string
-           (coerce (loop for code across (string-to-octets subject
-                                                           :external-format external-format)
-                      collect (code-char code))
-                   'string))))
-
-
-(defun send-noreply-mail (receiver subject body &rest bindings)
-  (send-mail (list receiver)
-             (acons "From"
-                    *noreply-mail-account*
-                    (acons "Subject"
-                           (prepare-subject subject)
-                           nil))
-             (typecase body
-               (pathname (restas:expand-file body (alexandria:plist-alist bindings)))
-               (string (restas:expand-text body (alexandria:plist-alist bindings)))
-               (otherwise (error "bad mail body: ~A" body)))))
-
-;;; check html form support
-
-(defun form-error-message (form field text)
-  (let ((el (xtree:make-element "div")))
-    (setf (xtree:attribute-value el "class") "error-info")
-    (setf (xtree:text-content el) text)
-    (xtree:insert-child-before el
-                               (or (xpath:find-single-node form
-                                                           (format nil "//input[@name='~A']" field))
-                                   (error "bad filed: ~A" field)))))
-
-
-(defun form-field-value (formdata field)
-  (cdr (assoc field formdata :test #'string=)))
-
-(defun form-field-empty-p (formdata field)
-  (string= (form-field-value formdata field) ""))
-
-(defun fill-form (form formdata)
-  (iter (for field in-xpath-result "//input|//textarea" on form)
-        (let ((field-value (form-field-value formdata
-                                             (xtree:attribute-value field "name"))))
-          (if field-value
-              (cond
-                ((string= (xtree:local-name field) "textarea") (setf (xtree:text-content field) 
-                                                                     (xtree:encode-special-chars nil field-value)))
-                (t (setf (xtree:attribute-value field "value") 
-                         (xtree:encode-special-chars nil field-value)))))))
-  form)
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defparameter *static-path* (merge-pathnames "static/" *basepath*))
@@ -113,6 +17,10 @@
   (merge-pathnames path *static-path*))
 
 (postmodern:defprepared user-theme* "SELECT theme FROM users WHERE login = $1" :single)
+
+(defmacro with-rulisp-db (&body body)
+  `(postmodern:with-connection *rulisp-db*
+     ,@body))
 
 (defun user-theme (name)
   (let ((theme (if name
