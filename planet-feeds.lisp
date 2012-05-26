e81343626bf9665f01f032fcdcd234ad15a88433
Moskvitin Andrey <archimag@gmail.com>
2012-05-27 02:31:14 +0400

    update planet feeds

--- planet-feeds.lisp
+++ planet-feeds.lisp
@@ -1,9 +1,61 @@
 
+(feed "http://lisplearn.blogspot.com/feeds/posts/default")
 
-;;(feed "http://13-49.livejournal.com/data/atom" :category "lisp")
-;;(feed "http://swizard.livejournal.com/data/atom" :category "lisp")
-;;(feed "http://lisp.tomsk.ru/wordpress/feed/")
+(feed "http://lisp-univ-etc.blogspot.com/feeds/posts/default/-/lisp/ru")
 
-;;(feed "/home/andrey/tmp/feeds/13-49.xml" :category "lisp")
-;;(feed "/home/andrey/tmp/feeds/tomsk.rss")
-(feed "/home/andrey/tmp/feeds/atom.xml")
+(feed "http://catap.ru/feeds/atom/tag/lisp/")
+
+(feed "http://alexott-ru.blogspot.com/feeds/posts/default/-/lisp")
+
+(feed "http://archimag.lisper.ru/feeds/atom/tag/lisp")
+
+(feed "http://dmitry-vk.livejournal.com/data/rss"
+      :category "lisp")
+
+(feed "http://lispnik.livejournal.com/data/rss"
+      :category "lisp")
+
+(feed "http://swizard.livejournal.com/data/rss"
+      :category "lisp")
+
+(feed "http://carcaddar.blogspot.com/feeds/posts/default/-/lisp-ru")
+
+(feed "http://andy128k.blogspot.com/feeds/posts/default/-/lisp")
+
+(feed "http://13-49-ru.blogspot.com/feeds/posts/default/-/lisp")
+
+(feed "http://axiger.livejournal.com/data/atom"
+      :category "lisp")
+
+(feed "http://cl-cad.blogspot.com/feeds/posts/default/-/lisp")
+
+(feed "http://astoon.livejournal.com/data/atom"
+      :category "lisp")
+
+(feed "http://dsorokin.blogspot.com/feeds/posts/default/-/lisp")
+
+(feed "http://uhalov.blogspot.com/feeds/posts/default/-/lisp")
+
+(feed "http://gzip4ever.blogspot.com/feeds/posts/default/-/lisp")
+
+(feed "http://turtle-bazon.blogspot.com/feeds/posts/default/-/lisp")
+
+(feed "http://keyfighter.blogspot.com/feeds/posts/default/-/lisp")
+
+(feed "http://bredogen.blogspot.com/feeds/posts/default")
+
+(feed "http://linkfly.livejournal.com/data/rss" 
+       :category "lisp")
+
+(feed "http://bajaz.livejournal.com/data/rss" 
+       :category "лисп")
+
+(feed "http://filonenko-mikhail.blogspot.com/feeds/posts/default/-/common%20lisp")
+
+(feed "http://smokeman85.blogspot.com/feeds/posts/default/-/lisp")
+
+(feed "http://bioh.livejournal.com/data/rss"
+      :category "lisp")
+
+(feed "http://dmitriy.budashny.com/tagged/Common-Lisp/rss")
+
